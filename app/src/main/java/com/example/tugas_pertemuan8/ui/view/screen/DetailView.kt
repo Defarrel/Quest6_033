c9e8b5d0f738ad47cdc462670f6e9eb9d309cfbd
defarrel <Defarrel>
2024-11-20 20:03:24 +0700

    Membuat parameter di DetailView

--- app/src/main/java/com/example/tugas_pertemuan8/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/example/tugas_pertemuan8/ui/view/screen/DetailView.kt
@@ -1,2 +1,16 @@
 package com.example.tugas_pertemuan8.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.tugas_pertemuan8.model.Mahasiswa
+import com.example.tugas_pertemuan8.model.RencanaStudi
+
+@Composable
+fun DetailView(
+    dataMhs: Mahasiswa,
+    dataKRS: RencanaStudi,
+    modifier: Modifier = Modifier,
+    onBackButton: () -> Unit
+) {
+
+}
