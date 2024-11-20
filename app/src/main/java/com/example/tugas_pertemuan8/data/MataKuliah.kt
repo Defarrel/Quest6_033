a7f3e7467550a9b964062b80b7855384c75262c8
defarrel <Defarrel>
2024-11-20 19:30:39 +0700

    Menambahkan object MataKuliah

--- app/src/main/java/com/example/tugas_pertemuan8/data/MataKuliah.kt
+++ app/src/main/java/com/example/tugas_pertemuan8/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.tugas_pertemuan8.data
 
 object MataKuliah {
+    val option = listOf(
+        "Pemprograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
 }
