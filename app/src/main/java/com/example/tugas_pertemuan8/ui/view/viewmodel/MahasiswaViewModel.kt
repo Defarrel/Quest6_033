7adcee0f206b9b37776a52055d709a1040801008
defarrel <Defarrel>
2024-11-20 19:51:33 +0700

    Mengisi MahasiswaViewModel dengan data dari model mahasiswa

--- app/src/main/java/com/example/tugas_pertemuan8/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/tugas_pertemuan8/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.tugas_pertemuan8.ui.view.viewmodel
 
+import com.example.tugas_pertemuan8.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
 class MahasiswaViewModel {
+    //only access internal class
+    private val _dataModel = MutableStateFlow(Mahasiswa())
+
+    //access for public
+    val dataModel: StateFlow<Mahasiswa> = _dataModel.asStateFlow()
+
+    fun saveDataMhs(
+        listData: MutableList<String>
+    ) {
+        _dataModel.update {mhs ->
+            mhs.copy(
+                nim = listData[0],
+                nama = listData[1],
+                email = listData[2],
+            )
+        }
+    }
 }
